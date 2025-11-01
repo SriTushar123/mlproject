131fa5723610b1a9cecd8a0f35d1f2babf97eb39
Tushar Srivastava <tusharsrivastava354@gmail.com>
2025-11-01 14:11:47 +0530

    Flask Application

--- src/utils.py
+++ src/utils.py
@@ -29,7 +29,13 @@ def evaluate_models(X_train,X_test,y_train,y_test,models):
 
     return report
 
-
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return pickle.load(file_obj)
+        
+    except Exception as e:
+        raise CustomException(e,sys)
 
 
 
